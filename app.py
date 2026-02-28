cb93db343427dc7070edd7d0d4fdcccea5641ea2
Abdul Kalam <abdulkalam0217@gmail.com>
2026-02-28 16:25:52 +0530

    Fixed DB port issue

--- app.py
+++ app.py
@@ -55,14 +55,18 @@ serializer = URLSafeTimedSerializer(app.secret_key)
 
 
 
+import os
+import pymysql
+
 def get_db_connection():
-    return MySQLdb.connect(
-        host=os.environ["MYSQLHOST"],
-        user=os.environ["MYSQLUSER"],
-        password=os.environ["MYSQLPASSWORD"],
-        database=os.environ["MYSQLDATABASE"],
+    connection = pymysql.connect(
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER"),
+        password=os.environ.get("MYSQLPASSWORD"),
+        database=os.environ.get("MYSQLDATABASE"),
         port=int(os.environ.get("MYSQLPORT", 3306))
     )
+    return connection
 
 # ==============================
 # Home Route
